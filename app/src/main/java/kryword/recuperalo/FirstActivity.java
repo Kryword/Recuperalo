6350d663ae457ee78150504e2d87aedd618f7dfb
Kryword <kryword@gmail.com>
2018-02-17 20:07:13 +0100

    Pantalla con mapa creado.

--- app/src/main/java/kryword/recuperalo/FirstActivity.java
+++ app/src/main/java/kryword/recuperalo/FirstActivity.java
@@ -1,7 +1,9 @@
 package kryword.recuperalo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.Iterator;
 import java.util.TreeSet;
@@ -12,5 +14,10 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    public void encontrado(View view){
+        Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+        startActivity(intent);
     }
 }
