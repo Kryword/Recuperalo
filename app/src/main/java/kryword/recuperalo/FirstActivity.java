0dc7fc16a109a4621b06e6777af42248d9ecdd68
Kryword <kryword@gmail.com>
2018-02-17 19:07:12 +0100

    Primera versión de la aplicación, no está implementado nada más que la pantalla inicial.

---
+++ app/src/main/java/kryword/recuperalo/FirstActivity.java
@@ -0,0 +1,16 @@
+package kryword.recuperalo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
