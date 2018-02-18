5e7dd47c12aba93bd4f147a696f4fda0b1f64f58
Kryword <kryword@gmail.com>
2018-02-18 21:36:49 +0100

    Añadida opción para poner nuevos objetos. A través del botón encontrado

---
+++ app/src/main/java/kryword/recuperalo/FoundActivity.java
@@ -0,0 +1,45 @@
+package kryword.recuperalo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class FoundActivity extends AppCompatActivity {
+    Double lat, lon;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_found);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        lat = bundle.getDouble("lat");
+        lon = bundle.getDouble("lot");
+    }
+
+    public void publicar(View view){
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+        bundle.putDouble("lat", lat);
+        bundle.putDouble("long", lon);
+        EditText title = (EditText) findViewById(R.id.title);
+        EditText description = (EditText) findViewById(R.id.description);
+        bundle.putString("title", title.getText().toString());
+        bundle.putString("description", description.getText().toString());
+        intent.putExtras(bundle);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    public void cancelar(View view){
+        onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+}
--- app/src/main/java/kryword/recuperalo/MapFoundActivity.java
+++ app/src/main/java/kryword/recuperalo/MapFoundActivity.java
@@ -1,5 +1,6 @@
 package kryword.recuperalo;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,7 +31,12 @@ public class MapFoundActivity extends AppCompatActivity {
                 mapboxMap.addOnMapLongClickListener(new MapboxMap.OnMapLongClickListener() {
                     @Override
                     public void onMapLongClick(@NonNull LatLng point) {
-                        mapboxMap.addMarker(new MarkerOptions().title("Title").position(point));
+                        Intent intent = new Intent(getApplicationContext(), FoundActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putDouble("lat", point.getLatitude());
+                        bundle.putDouble("long", point.getLongitude());
+                        intent.putExtras(bundle);
+                        startActivityForResult(intent, RESULT_OK);
                     }
                 });
             }
@@ -40,5 +46,20 @@ public class MapFoundActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            final LatLng pos = new LatLng(bundle.getDouble("lat"), bundle.getDouble("long"));
+            final String title = bundle.getString("title");
+            mapView.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(final MapboxMap mapboxMap) {
+                    mapboxMap.addMarker(new MarkerOptions().setTitle(title).setPosition(pos));
+                }
+            });
+        }
     }
 }
