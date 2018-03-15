42dcfe9bb0dfc9e2e12bac999cb3e6a71a7d4083
Kryword <kryword@gmail.com>
2018-03-15 22:49:03 +0100

    Añadida geolocalización y un botón en el mapa para obligar al mapa a actualizarse. Además de eso ahora los puntos aparecen automáticamente en el mapa.

--- app/src/main/java/kryword/recuperalo/MapLostActivity.java
+++ app/src/main/java/kryword/recuperalo/MapLostActivity.java
@@ -1,16 +1,16 @@
 package kryword.recuperalo;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.support.annotation.NonNull;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.Toast;
 
-import com.google.gson.JsonObject;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
@@ -19,71 +19,65 @@ import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
-import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-import com.mapbox.services.commons.geojson.Feature;
-import com.mapbox.services.commons.geojson.Point;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
-import java.io.InputStream;
 import java.util.List;
 
 import kryword.recuperalo.Modelos.Punto;
 
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textColor;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textField;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textSize;
-
 public class MapLostActivity extends AppCompatActivity {
+
+    private final double ZOOM = 14f;
 
     MapView mapView;
     MainApplication ma;
+    boolean newPos; // Esto me sirve para mantener el conocimiento de si hay que actualizar posición de cámara o no
+    LatLng position;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_lost);
+
+        // Posición por defecto, es el centro de Pamplona.
+        position = new LatLng(42.8157961, -1.6675312);
+        newPos = true;
+
         ma = (MainApplication) this.getApplicationContext();
         getList();
+
         Mapbox.getInstance(this, getString(R.string.access_token));
-        mapView = (MapView)findViewById(R.id.mapView);
+        mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(final MapboxMap mapboxMap) {
-
-                    mapboxMap.addImage(
-                            "my-marker-image",
-                            BitmapFactory.decodeResource(MapLostActivity.this.getResources(),
-                                    R.drawable.mapbox_marker_icon_default)
-                    );
-                    for (Punto punto : ma.list) {
-                        mapboxMap.addMarker(new MarkerOptions().position(punto.getPos()).title(punto.getData().getTitle()).setSnippet(punto.getData().getDescription()));
+                mapboxMap.addImage(
+                        "my-marker-image",
+                        BitmapFactory.decodeResource(MapLostActivity.this.getResources(),
+                                R.drawable.mapbox_marker_icon_default)
+                );
+                mapboxMap.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        Intent intent = new Intent(getApplicationContext(), MarkerActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("title", marker.getTitle());
+                        bundle.putString("description", marker.getSnippet());
+                        bundle.putDouble("long", marker.getPosition().getLongitude());
+                        bundle.putDouble("lat", marker.getPosition().getLatitude());
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                        return true;
                     }
-
-                    // Move camera over points
-                    mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(42.81098654188819, -1.6432714462280273), 14));
-
-                    mapboxMap.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
-
-                        @Override
-                        public boolean onMarkerClick(Marker marker) {
-                            Intent intent = new Intent(getApplicationContext(), MarkerActivity.class);
-                            Bundle bundle = new Bundle();
-                            bundle.putString("title", marker.getTitle());
-                            bundle.putString("description", marker.getSnippet());
-                            bundle.putDouble("long", marker.getPosition().getLongitude());
-                            bundle.putDouble("lat", marker.getPosition().getLatitude());
-                            intent.putExtras(bundle);
-                            startActivity(intent);
-                            return true;
-                        }
-                    });
+                });
             }
         });
+
+        // Actualizo mapa
+        updateMap();
     }
 
     @Override
@@ -97,16 +91,56 @@ public class MapLostActivity extends AppCompatActivity {
             public void done(List<Punto> objects, ParseException e) {
                 if (e == null) {
                     ma.list = objects;
-                    Log.v("query OK ", "getServerList()");
+                    Log.v("query OK ", "getList()");
+                    updateMap();
                 } else {
-                    Log.v("error query, reason: " + e.getMessage(), "getServerList()");
-                    Toast.makeText(
-                            getBaseContext(),
-                            "getServerList(): error  query, reason: " + e.getMessage(),
-                            Toast.LENGTH_SHORT).show();
+                    Log.v("error query, reason: " + e.getMessage(), "getList()");
                 }
             }
         });
+    }
 
+    private void updateMap() {
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                Toast.makeText(getBaseContext(), "Actualizado el mapa", Toast.LENGTH_SHORT).show();
+                for (Punto punto : ma.list) {
+                    mapboxMap.addMarker(new MarkerOptions().position(punto.getPos()).title(punto.getData().getTitle()).setSnippet(punto.getData().getDescription()));
+                }
+                if (newPos) {
+                    mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, ZOOM));
+                }
+            }
+        });
+    }
+
+    public void updatePosition(View view){
+        Location location = getLastKnownLocation();
+        if (location != null) {
+            Log.v("LOCATION", "Encontrada localización: " + location);
+            position.setLatitude(location.getLatitude());
+            position.setLongitude(location.getLongitude());
+            newPos = true;
+            updateMap();
+        }
+    }
+
+    private Location getLastKnownLocation() {
+        LocationManager mLocationManager = (LocationManager)getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            // Habría que modificar este SupressLint por otra cosa para mantener la estabilidad.
+            @SuppressLint("MissingPermission") Location l = mLocationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Encontrada la mejor localización
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
     }
 }
