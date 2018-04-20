a7a003b787cc86e0bf47f45bcd7b776582475af4
Kryword <kryword@gmail.com>
2018-04-21 00:16:03 +0200

    Modificadas vistas para seguir un estilo más "material design"

--- app/src/main/java/kryword/recuperalo/MarkerActivity.java
+++ app/src/main/java/kryword/recuperalo/MarkerActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -26,6 +27,9 @@ public class MarkerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marker);
+
+        ((TextView)findViewById(R.id.description)).setMovementMethod(new ScrollingMovementMethod());
+
         ma = (MainApplication)getApplicationContext();
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -42,30 +46,36 @@ public class MarkerActivity extends AppCompatActivity {
         // Relleno los campos del título y la descripción con los datos obtenidos del objeto
         title.setText(objTitle);
         description.setText(objDescription);
-        Mapbox.getInstance(this, getString(R.string.access_token));
         Log.i("Datos del objeto:", position + "; " + objTitle + "; " + objDescription);
+
+        Mapbox.getInstance(this, getString(R.string.access_token));
         mapView = (MapView)findViewById(R.id.viewMap);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(final MapboxMap mapboxMap) {
-                mapboxMap.addImage(
-                        "my-marker-image",
-                        BitmapFactory.decodeResource(MarkerActivity.this.getResources(),
-                                R.drawable.mapbox_marker_icon_default)
-                );
-                mapboxMap.addMarker(new MarkerOptions()
-                        .position(position)
-                        .title(objTitle)
-                        .setSnippet(objDescription));
-                mapboxMap.moveCamera(CameraUpdateFactory.newLatLng(position));
-            }
-        });
+
+        if (mapView != null) {
+            mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(final MapboxMap mapboxMap) {
+                    mapboxMap.addImage(
+                            "my-marker-image",
+                            BitmapFactory.decodeResource(MarkerActivity.this.getResources(),
+                                    R.drawable.mapbox_marker_icon_default)
+                    );
+                    mapboxMap.addMarker(new MarkerOptions()
+                            .position(position)
+                            .title(objTitle)
+                            .setSnippet(objDescription));
+                    mapboxMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+                }
+            });
+        }
     }
 
     @Override
     public void onBackPressed() {
-        mapView.onDestroy();
+        if (mapView != null) {
+            mapView.onDestroy();
+        }
         finish();
     }
 }
